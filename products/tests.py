3b1b0870c966c4c22faacbf0553570f64b0b85cb
cthoms1993 <50752438+cthoms1993@users.noreply.github.com>
2020-06-06 18:35:09 +0100

    added product model and model test

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Product
+
 
 # Create your tests here.
+class ProductTests(TestCase):
+    """here we will define a test for the products we will be using"""
+
+    def test_str(self):
+        test_name = Product(name='A product')
+        self.assertEqual(str(test_name), 'A product')
