ce38bc550ba81a8002d8322804493e467ebad372
cthoms1993 <50752438+cthoms1993@users.noreply.github.com>
2020-06-09 14:53:45 +0100

    added html page for viewing products in users cart

--- voicetechms4/urls.py
+++ voicetechms4/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import url, include
 from accounts import urls as urls_accounts
 from home.views import index
 from products import urls as urls_products
+from cart import urls as urls_cart
 from products.views import all_products
 from django.views import static
 from .settings import MEDIA_ROOT
@@ -28,6 +29,7 @@ urlpatterns = [
     url(r'^index/', index, name="index"),
     url(r'^accounts/', include(urls_accounts)),
     url(r'^products/', include(urls_products)),
+    url(r'^cart/', include(urls_cart)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
 
 ]
