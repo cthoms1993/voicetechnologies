ffe7e63054e8f00d6bcc80318a8bcb8aba3bde6f
cthoms1993 <50752438+cthoms1993@users.noreply.github.com>
2020-06-11 22:22:57 +0100

    changed urls.py and image source to display uploaded images.

--- voicetechms4/urls.py
+++ voicetechms4/urls.py
@@ -23,16 +23,19 @@ from cart import urls as urls_cart
 from search import urls as urls_search
 from products.views import all_products
 from .settings import MEDIA_ROOT
+from django.conf import settings
 from django.views import static
+from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', all_products, name='index'),
-    url(r'^index/', index, name="index"),
-    url(r'^accounts/', include(urls_accounts)),
-    url(r'^products/', include(urls_products)),
-    url(r'^cart/', include(urls_cart)),
-    url(r'^search/', include(urls_search)),
-    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
+                  url(r'^admin/', admin.site.urls),
+                  url(r'^$', all_products, name='index'),
+                  url(r'^index/', index, name="index"),
+                  url(r'^accounts/', include(urls_accounts)),
+                  url(r'^products/', include(urls_products)),
+                  url(r'^cart/', include(urls_cart)),
+                  url(r'^search/', include(urls_search)),
 
-]
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG: # new
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
