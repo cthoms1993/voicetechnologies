3bc09f10b071231a0355a73b110463daafd9ae94
cthoms1993 <50752438+cthoms1993@users.noreply.github.com>
2020-06-16 15:18:28 +0100

    added model to allow commenting on blog posts

--- blog/models.py
+++ blog/models.py
@@ -16,3 +16,18 @@ class Post(models.Model):
 
     def __unicode__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateField(auto_now=True)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'comment {} by {}'.format(self.body, self.name)
