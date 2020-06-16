61572073215c09fe3266ef0fcc1035197100bfeb
cthoms1993 <50752438+cthoms1993@users.noreply.github.com>
2020-06-16 15:47:38 +0100

    edited postdetail view to show comments made by other users

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import CommentForm
 
 
 # Create your views here.
@@ -24,4 +25,15 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.views += 1
     post.save()
-    return render(request, "postdetail.html", {'post': post})
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, "postdetail.html",
+                  {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form})
