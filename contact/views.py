55441eb269857f3086c8babb62ada38935db2af6
cthoms1993 <50752438+cthoms1993@users.noreply.github.com>
2020-06-17 15:31:50 +0100

    edited contact view to send admins email when contact form submitted

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import ContactForm
 from django.contrib import messages
+from django.core.mail import mail_admins
 
 
 # Create your views here.
@@ -8,6 +9,12 @@ def contact(request):
     if request.method == 'POST':
         c = ContactForm(request.POST)
         if c.is_valid():
+            name = c.cleaned_data['name']
+            sender = c.cleaned_data['email']
+            subject = "You have a new contact form from {}:{}".format(name, sender)
+            message = "Subject: {}\n\nMessage: {}".format(c.cleaned_data['subject'], c.cleaned_data['message'])
+            mail_admins(subject, message)
+
             c.save()
             messages.add_message(request, messages.INFO, 'Your contact form has been submitted')
             return redirect('contact')
