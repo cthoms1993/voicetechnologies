6078026c09955e3aa509feb61fbb20e2f5b21b2f
cthoms1993 <50752438+cthoms1993@users.noreply.github.com>
2020-06-17 14:22:32 +0100

    added contact model to admin page.

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Contact
 
 # Register your models here.
+admin.site.register(Contact)
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'date',)
+    search_fields = ('name', 'email',)
+    date_hierarchy = 'date'
--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.contrib import messages
+
 
 # Create your views here.
+def contact(request):
+    if request.method == 'POST':
+        c = ContactForm(request.POST)
+        if c.is_valid():
+            c.save()
+            messages.add_message(request, messages.INFO, 'Your contact form ahs been submitted')
+            return redirect('contact')
+    else:
+        c = ContactForm()
+    return render(request, 'contact.html', {'form': c})
