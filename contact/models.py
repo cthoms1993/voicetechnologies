d3f698a0d8369eaa97645b6f2103de61cc72548e
cthoms1993 <50752438+cthoms1993@users.noreply.github.com>
2020-06-17 13:26:04 +0100

    added model for contact page to allow user to contact the site

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Feedback(models.Model):
+    name = models.CharField(max_length=200, help_text="name of sender")
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "feedback"
+
+    def __str__(self):
+        return self.name + "-" + self.email
