ae606e320afdf5dc044374691846d42c195e5eca
cthoms1993 <50752438+cthoms1993@users.noreply.github.com>
2020-06-18 18:12:26 +0100

    made changed to top level urls to try fix google chrome error

--- voicetechms4/urls.py
+++ voicetechms4/urls.py
@@ -18,29 +18,27 @@ from django.contrib import admin
 from django.conf.urls import url, include
 from accounts import urls as urls_accounts
 from home.views import index
-from products import urls as urls_products
 from cart import urls as urls_cart
 from search import urls as urls_search
-from products.views import all_products
 from checkout import urls as urls_checkout
 from django.conf import settings
 from django.conf.urls.static import static
-from staff.views import all_staff
 from blog import urls as urls_blog
 from contact import urls as urls_contact
+from products import urls as urls_products
+from staff import urls as urls_staff
 
 urlpatterns = [
                   url(r'^admin/', admin.site.urls),
-                  url(r'^$', all_products, name='index'),
-                  url(r'^index/', index, name="index"),
+                  url(r'^$', index, name='index'),
                   url(r'^accounts/', include(urls_accounts)),
                   url(r'^products/', include(urls_products)),
                   url(r'^cart/', include(urls_cart)),
                   url(r'^search/', include(urls_search)),
                   url(r'^checkout/', include(urls_checkout)),
-                  url(r'^about/', all_staff, name='about'),
                   url(r'^blog/', include(urls_blog)),
                   url(r'^contact/', include(urls_contact)),
+                  url(r'^staff/', include(urls_staff)),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
