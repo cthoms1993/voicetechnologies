911ccb30719312b92ddc99200d03c59742eaf2a7
cthoms1993 <50752438+cthoms1993@users.noreply.github.com>
2020-06-22 16:15:24 +0100

    added information for three products that are being displayed.

--- products/views.py
+++ products/views.py
@@ -6,3 +6,15 @@ from .models import Product
 def all_products(request):
     products = Product.objects.all()
     return render(request, "products.html", {"products": products})
+
+
+def winvoicepro(request):
+    return render(request, "winvoicepro.html")
+
+
+def winvoiceweb(request):
+    return render(request, "winvoiceweb.html")
+
+
+def formstream(request):
+    return render(request, "formstream.html")
