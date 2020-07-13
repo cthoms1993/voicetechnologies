9ab0fc6bf9ce1ddec1caf1596286bce77a1cbf33
cthoms1993 <50752438+cthoms1993@users.noreply.github.com>
2020-07-13 14:33:10 +0100

    changed layout  for store page on different screen sizes.

--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
-    description = models.TextField()
+    description = models.TextField(max_length=500)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images', default='')
 
