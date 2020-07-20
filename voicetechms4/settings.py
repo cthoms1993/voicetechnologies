e596f7fe3ea654766d9cfcb59b20370e8dbfc1e2
cthoms1993 <50752438+cthoms1993@users.noreply.github.com>
2020-07-20 16:51:38 +0100

    made changes to static settings to see if heroku integration works.

--- voicetechms4/settings.py
+++ voicetechms4/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import dj_database_url
 import os
-
 if os.path.exists('env.py'):
     import env
-import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -144,9 +143,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles/static'),)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'voicetechms4/static')
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "/media/"
